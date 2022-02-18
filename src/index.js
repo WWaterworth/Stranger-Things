e6bd49b65a14ed3549cf08adb47c2dd96ba4b38c
Warren Waterworth <warren.waterworth@gmail.com>
2022-02-17 19:34:22 -0700

    App renders posts to browser from API

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from 'react';
+import ReactDOM, {BrowserRouter as Router} from 'react-dom';
+const App = () => {
+  const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const resp = await fetch("https://strangers-things.herokuapp.com/api/2111-CSU-RM-WEB-PT/posts/");
+      const result = await resp.json();
+      const postData = result.data.posts;
+      setPosts(postData);
+    }
+    fetchPosts();
+  },[])
+
+  return (<>
+  <h1>Stranger Things</h1>
+  <h2>Posts</h2>
+  {posts && posts.map((elem, idx)=> {
+    return (
+      <div key={idx}>
+        <h3>{elem.title}</h3>
+        <p>Seller: {elem.author.username}</p>
+        <h4>Location: {elem.location}</h4>
+        <p>{elem.description}</p>
+        <p>Price: {elem.price}</p>
+      </div>
+    )
+  })}
+  </>
+  )
+}
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
